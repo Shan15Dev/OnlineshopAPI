46ad7f66cc61bcc319064fff80c69dec971279dd
zmuras <zmuras@bbcag.ch>
2022-05-01 16:33:56 +0200

    Brand Repository and interface added

---
+++ OnlineShopAPI/Repository/BrandRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Concurrent;
+using System.Net.Sockets;
+using OnlineShopAPI.Model;
+
+namespace OnlineShopAPI.Repository;
+
+public class BrandRepository : IBrandRepository
+{
+    private static ConcurrentDictionary<string, BrandModel> _brand = new ConcurrentDictionary<string, BrandModel>();
+
+
+    public void Add(BrandModel brandModel)
+    {
+        brandModel.Id = Guid.NewGuid().ToString();
+        _brand[brandModel.Id] = brandModel;
+    }
+
+    public IEnumerable<BrandModel> GetAll()
+    {
+        return _brand.Values;
+    }
+
+    public BrandModel Find(string key)
+    {
+        BrandModel brandModel;
+        _brand.TryGetValue(key, out brandModel);
+        return brandModel;
+    }
+
+    public BrandModel Remove(string key)
+    {
+        BrandModel brandModel;
+        _brand.TryGetValue(key, out brandModel);
+        _brand.TryRemove(key, out brandModel);
+        return brandModel;
+    }
+
+    public void Update(BrandModel brandModel)
+    {
+        _brand[brandModel.Id] = brandModel;
+    }
+}
---
+++ OnlineShopAPI/Repository/IBrandRepository.cs
@@ -0,0 +1,12 @@
+﻿using OnlineShopAPI.Model;
+
+namespace OnlineShopAPI.Repository;
+
+public interface IBrandRepository
+{
+    void Add(BrandModel brandModel);
+    IEnumerable<BrandModel> GetAll();
+    BrandModel Find(string key);
+    BrandModel Remove(string key);
+    void Update(BrandModel item);
+}
