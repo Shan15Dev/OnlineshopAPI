f67556f1b68a90558f539e2852cef38d0fc3dd2a
zmuras <zmuras@bbcag.ch>
2022-05-01 10:36:33 +0200

    StorageModel added

--- OnlineShopAPI/Model/StorageModel.cs
+++ OnlineShopAPI/Model/StorageModel.cs
@@ -1,6 +1,10 @@
-﻿namespace OnlineShopAPI.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopAPI.Model;
 
 public class StorageModel
 {
-    
+    [Key] public int Id { get; set; }
+    [Required] [MaxLength(45)] public string Storage { get; set; }
+    [Required] [MaxLength(45)] public string Place { get; set; }
 }
